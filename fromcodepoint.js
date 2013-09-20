7d205c23898be8b57f6c86616f8b87ee96f439b7
Mathias Bynens <mathias@qiwi.be>
2013-09-20 13:41:58 +0200

    Rename `codePoints` into `codeUnits` as that’s more accurate

--- fromcodepoint.js
+++ fromcodepoint.js
@@ -1,6 +1,6 @@
 if (!String.fromCodePoint) {
 	String.fromCodePoint = function() {
-		var codePoints = [];
+		var codeUnits = [];
 		var floor = Math.floor;
 		var highSurrogate;
 		var lowSurrogate;
@@ -20,15 +20,15 @@ if (!String.fromCodePoint) {
 				throw RangeError('Invalid code point: ' + codePoint);
 			}
 			if (codePoint <= 0xFFFF) { // BMP code point
-				codePoints.push(codePoint);
+				codeUnits.push(codePoint);
 			} else { // Astral code point; split in surrogate halves
 				// http://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
 				codePoint -= 0x10000;
 				highSurrogate = (codePoint >> 10) + 0xD800;
 				lowSurrogate = (codePoint % 0x400) + 0xDC00;
-				codePoints.push(highSurrogate, lowSurrogate);
+				codeUnits.push(highSurrogate, lowSurrogate);
 			}
 		}
-		return String.fromCharCode.apply(null, codePoints);
+		return String.fromCharCode.apply(null, codeUnits);
 	};
 }
