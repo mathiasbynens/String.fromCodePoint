3976b0fd561c4550d838c05fabff6ee285a6719c
Mathias Bynens <mathias@qiwi.be>
2013-09-20 13:29:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# ES6 `String.fromCodePoint` polyfill
+
+A robust & optimized polyfill for [the `String.fromCodePoint` method in ECMAScript 6](http://people.mozilla.org/~jorendorff/es6-draft.html#sec-21.1.2.2).
+
+Other polyfills for `String.fromCodePoint` are available:
+
+* <http://norbertlindenberg.com/2012/05/ecmascript-supplementary-characters/> by [Norbert Lindenberg](http://norbertlindenberg.com/) (passes all tests)
+* <https://gist.github.com/slevithan/2290602> by [Steven Levithan](http://stevenlevithan.com/) (fails 8 tests)
+
+## Installation
+
+In a browser:
+
+```html
+<script src="fromCodePoint.js"></script>
+```
+
+Via [npm](http://npmjs.org/):
+
+```bash
+npm install string.fromcodepoint
+```
+
+Then, in [Node.js](http://nodejs.org/):
+
+```js
+require('String.fromCodePoint');
+```
+
+## Author
+
+| [![twitter/mathias](http://gravatar.com/avatar/24e08a9ea84deb17ae121074d0f17125?s=70)](http://twitter.com/mathias "Follow @mathias on Twitter") |
+|---|
+| [Mathias Bynens](http://mathiasbynens.be/) |
+
+## License
+
+This polyfill is available under the [MIT](http://mths.be/mit) license.
---
+++ tests/tests.js
@@ -0,0 +1,102 @@
+var assert = require('assert');
+
+require('../fromcodepoint.js');
+
+var tests = [
+	{
+		'codePoints': [0x1D306],
+		'result': '\uD834\uDF06'
+	},
+	{
+		'codePoints': [0x1D306, 0x61, 0x1D307],
+		'result': '\uD834\uDF06a\uD834\uDF07'
+	},
+	{
+		'codePoints': [0x61, 0x62, 0x1D307],
+		'result': 'ab\uD834\uDF07'
+	},
+	{
+		'codePoints': [],
+		'result': ''
+	},
+	{
+		'codePoints': [0],
+		'result': '\0'
+	},
+	{
+		'codePoints': [undefined],
+		'error': RangeError
+	},
+	{
+		'codePoints': [null],
+		'result': '\0'
+	},
+	{
+		'codePoints': [false],
+		'result': '\0'
+	},
+	{
+		'codePoints': [NaN],
+		'error': RangeError
+	},
+	{
+		'codePoints': [Infinity],
+		'error': RangeError
+	},
+	{
+		'codePoints': [''],
+		'result': '\0'
+	},
+	{
+		'codePoints': ['_'],
+		'error': RangeError
+	},
+	{
+		'codePoints': ['+Infinity'],
+		'error': RangeError
+	},
+	{
+		'codePoints': ['-Infinity'],
+		'error': RangeError
+	},
+	{
+		'codePoints': [3.14],
+		'error': RangeError
+	},
+	{
+		'codePoints': [3e-2],
+		'error': RangeError
+	},
+];
+
+var errors = 0;
+tests.forEach(function(test, index) {
+	try {
+		if (test.error) {
+			assert['throws'](
+				function() {
+					String.fromCodePoint.apply(null, test.codePoints);
+				},
+				test.error,
+				index
+			);
+		} else {
+			assert.equal(
+				String.fromCodePoint.apply(null, test.codePoints),
+				test.result,
+				index
+			);
+		}
+	} catch(error) {
+		errors++;
+		console.log(error.message);
+	}
+});
+
+console.log(
+	'Ran %d tests.\n%d assertions failed.\n%d assertions passed.',
+	tests.length, errors, tests.length - errors
+);
+if (errors) {
+	process.exit(1);
+}
