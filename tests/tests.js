ffa1982ed8e03ab1afeb27cda088c3b553bf5d39
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-05-20 14:24:47 +0000

    Use `tape` in tests and run them on every Node.js version

--- tests/tests.js
+++ tests/tests.js
@@ -1,49 +1,63 @@
-var assert = require('assert');
-var assertEquals = assert.equal;
-var assertThrows = assert['throws'];
+'use strict';
 
-require('../auto.js');
+module.exports = function(fromCodePoint, t) {
+	t.equal(fromCodePoint(), '', 'no arguments');
 
-assertEquals(String.fromCodePoint.length, 1);
-assertEquals(String.propertyIsEnumerable('fromCodePoint'), false);
+	t.test('cast to 0', function(st) {
+		st.equal(fromCodePoint(''), '\0');
+		st.equal(fromCodePoint(-0), '\0');
+		st.equal(fromCodePoint(0), '\0');
+		st.equal(fromCodePoint(false), '\0');
+		st.equal(fromCodePoint(null), '\0');
+		st.end();
+	});
 
-assertEquals(String.fromCodePoint(''), '\0');
-assertEquals(String.fromCodePoint(), '');
-assertEquals(String.fromCodePoint(-0), '\0');
-assertEquals(String.fromCodePoint(0), '\0');
-assertEquals(String.fromCodePoint(0x1D306), '\uD834\uDF06');
-assertEquals(String.fromCodePoint(0x1D306, 0x61, 0x1D307), '\uD834\uDF06a\uD834\uDF07');
-assertEquals(String.fromCodePoint(0x61, 0x62, 0x1D307), 'ab\uD834\uDF07');
-assertEquals(String.fromCodePoint(false), '\0');
-assertEquals(String.fromCodePoint(null), '\0');
+	t.test('astral code points', function(st) {
+		st.equal(fromCodePoint(0x1D306), '\uD834\uDF06');
+		st.equal(fromCodePoint(0x1D306, 0x61, 0x1D307), '\uD834\uDF06a\uD834\uDF07');
+		st.equal(fromCodePoint(0x61, 0x62, 0x1D307), 'ab\uD834\uDF07');
+		st.end();
+	});
 
-assertThrows(function() { String.fromCodePoint('_'); }, RangeError);
-assertThrows(function() { String.fromCodePoint('+Infinity'); }, RangeError);
-assertThrows(function() { String.fromCodePoint('-Infinity'); }, RangeError);
-assertThrows(function() { String.fromCodePoint(-1); }, RangeError);
-assertThrows(function() { String.fromCodePoint(0x10FFFF + 1); }, RangeError);
-assertThrows(function() { String.fromCodePoint(3.14); }, RangeError);
-assertThrows(function() { String.fromCodePoint(3e-2); }, RangeError);
-assertThrows(function() { String.fromCodePoint(-Infinity); }, RangeError);
-assertThrows(function() { String.fromCodePoint(+Infinity); }, RangeError);
-assertThrows(function() { String.fromCodePoint(NaN); }, RangeError);
-assertThrows(function() { String.fromCodePoint(undefined); }, RangeError);
-assertThrows(function() { String.fromCodePoint({}); }, RangeError);
-assertThrows(function() { String.fromCodePoint(/./); }, RangeError);
-var tmp = 0x60;
-assertEquals(String.fromCodePoint({ 'valueOf': function() { ++tmp; return tmp; } }), 'a');
-assertEquals(tmp, 0x61);
+	t.test('invalid code poits', function(st) {
+		st['throws'](function() { fromCodePoint('_'); }, RangeError);
+		st['throws'](function() { fromCodePoint('+Infinity'); }, RangeError);
+		st['throws'](function() { fromCodePoint('-Infinity'); }, RangeError);
+		st['throws'](function() { fromCodePoint(-1); }, RangeError);
+		st['throws'](function() { fromCodePoint(0x10FFFF + 1); }, RangeError);
+		st['throws'](function() { fromCodePoint(3.14); }, RangeError);
+		st['throws'](function() { fromCodePoint(3e-2); }, RangeError);
+		st['throws'](function() { fromCodePoint(-Infinity); }, RangeError);
+		st['throws'](function() { fromCodePoint(+Infinity); }, RangeError);
+		st['throws'](function() { fromCodePoint(NaN); }, RangeError);
+		st['throws'](function() { fromCodePoint(undefined); }, RangeError);
+		st['throws'](function() { fromCodePoint({}); }, RangeError);
+		st['throws'](function() { fromCodePoint(/./); }, RangeError);
+		st.end();
+	});
 
-var counter = Math.pow(2, 15) * 3 / 2;
-var result = [];
-while (--counter >= 0) {
-	result.push(0); // one code unit per symbol
-}
-String.fromCodePoint.apply(null, result); // must not throw
+	t.test('cast code point', function(st) {
+		var tmp = 0x60;
+		st.equal(fromCodePoint({ 'valueOf': function() { ++tmp; return tmp; } }), 'a');
+		st.equal(tmp, 0x61);
+		st.end();
+	});
 
-var counter = Math.pow(2, 15) * 3 / 2;
-var result = [];
-while (--counter >= 0) {
-	result.push(0xFFFF + 1); // two code units per symbol
-}
-String.fromCodePoint.apply(null, result); // must not throw
+	t.test('long arguments list', function(st) {
+		var counter = Math.pow(2, 15) * 3 / 2;
+		var result = [];
+		while (--counter >= 0) {
+			result.push(0); // one code unit per symbol
+		}
+		fromCodePoint.apply(null, result); // must not throw
+
+		var counter = Math.pow(2, 15) * 3 / 2;
+		var result = [];
+		while (--counter >= 0) {
+			result.push(0xFFFF + 1); // two code units per symbol
+		}
+		fromCodePoint.apply(null, result); // must not throw
+
+		st.end();
+	});
+};
